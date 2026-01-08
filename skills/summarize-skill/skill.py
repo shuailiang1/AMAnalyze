62ca662ef0ad4f5cfa655cc1f946733fb67275cd
dongliang huang <dongliang.huang@shopee.com>
2026-01-08 11:47:45 +0800

    修复找不到 skill bug

---
+++ skills/summarize-skill/skill.py
@@ -0,0 +1,8 @@
+def run(trace_id: int) -> bool:
+    """
+    Summarize the successful agent execution trace.
+    """
+    print('---------------------------------')
+    print(trace_id)
+    print('---------------------------------')
+    return True
